4ec2777373d852e377f8a85dc36d8bab76754b9b
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-22 12:57:56 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useViewerRecord } from "@self.id/react";
 import styles from '../styles/Home.module.css'
 import { Web3Provider } from '@ethersproject/providers'
 import { useEffect, useRef, useState } from "react"
@@ -80,5 +79,46 @@ export default function Home() {
 }
 
 function RecordSetter() {
+  const record = useViewerRecord("basicProfile");
+
+ const updateRecordName = async (name) => {
+    await record.merge({
+      name: name,
+    });
+  };
+
+  const [name, setName] = useState("");
+  return (
+    <div className={styles.content}>
+      <div className={styles.mt2}>
+        {record.content ? (
+          <div className={styles.flexCol}>
+            <span className={styles.subtitle}>
+              Hello {record.content.name}!
+            </span>
+
+            <span>
+              The above name was loaded from Ceramic Network. Try updating it
+              below.
+            </span>
+          </div>
+        ) : (
+          <span>
+            You do not have a profile record attached to your 3ID. Create a
+            basic profile by setting a name below.
+          </span>
+        )}
+      </div>
+
+      <input
+        type="text"
+        placeholder="Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className={styles.mt2}
+      />
+      <button onClick={() => updateRecordName(name)}>Update</button>
+    </div>
+  );
     
 }
