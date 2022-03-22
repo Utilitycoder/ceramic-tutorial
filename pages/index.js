18bb5deeadb8e4d7a65d675a27ac688cf5111c7b
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-22 12:55:11 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,31 @@ import { Web3Provider } from '@ethersproject/providers'
 import { useEffect, useRef, useState } from "react"
 import Web3Modal from "web3modal"
 import { useViewerConnection } from "@self.id/react";
+import { EthereumAuthProvider } from "@self.id/web";
 
-const web3ModalRef = useRef();
 
-const getProvider = async () => {
+
+export default function Home() {
+  const web3ModalRef = useRef();
+
+  const getProvider = async () => {
     const provider = await web3ModalRef.current.connect();
     const wrappedProvider = new Web3Provider(provider);
     return wrappedProvider;
-}
+  }
 
-export default function Home() {
+  const connectToSelfID = async () => {
+  const ethereumAuthProvider = await getEthereumAuthProvider();
+  connect(ethereumAuthProvider);
+  };
+
+  const getEthereumAuthProvider = async () => {
+  const wrappedProvider = await getProvider();
+  const signer = wrappedProvider.getSigner();
+  const address = await signer.getAddress();
+  return new EthereumAuthProvider(wrappedProvider.provider, address);
+  };
+  
   const [connection, connect, disconnect] = useViewerConnection();
 
   useEffect(() => {
@@ -25,5 +40,45 @@ export default function Home() {
         disableInjectedProvider: false,
       });
     }
-}, [connection.status]);
+  }, [connection.status]);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.navbar}>
+        <span className={styles.title}>Ceramic Demo</span>
+        {connection.status === "connected" ? (
+          <span className={styles.subtitle}>Connected</span>
+        ) : (
+          <button
+            onClick={connectToSelfID}
+            className={styles.button}
+            disabled={connection.status === "connecting"}
+          >
+            Connect
+          </button>
+        )}
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.connection}>
+          {connection.status === "connected" ? (
+            <div>
+              <span className={styles.subtitle}>
+                Your 3ID is {connection.selfID.id}
+              </span>
+              <RecordSetter />
+            </div>
+          ) : (
+            <span className={styles.subtitle}>
+              Connect with your wallet to access your 3ID
+            </span>
+          )}
+        </div>
+      </div>
+    </div>
+)
+}
+
+function RecordSetter() {
+    
 }
