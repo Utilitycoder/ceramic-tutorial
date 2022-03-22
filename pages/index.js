d2d3e761651c8b9bdd88087fa2e94b28e5f9c058
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-22 12:43:29 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -5,5 +5,13 @@ import { Web3Provider } from '@ethersproject/providers'
 import { useEffect, useRef, useState } from "react"
 import Web3Modal from "web3modal"
 
+const web3ModalRef = useRef();
+
+const getProvider = async () => {
+    const provider = await web3ModalRef.current.connect();
+    const wrappedProvider = new Web3Provider(provider);
+    return wrappedProvider;
+}
+
 export default function Home() {
 }
