13b34c5f9957730b010408c5ef71e51200ee06fb
Utilitycoder <66361605+Utilitycoder@users.noreply.github.com>
2022-03-22 12:46:40 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css'
 import { Web3Provider } from '@ethersproject/providers'
 import { useEffect, useRef, useState } from "react"
 import Web3Modal from "web3modal"
+import { useViewerConnection } from "@self.id/react";
 
 const web3ModalRef = useRef();
 
@@ -14,4 +15,15 @@ const getProvider = async () => {
 }
 
 export default function Home() {
+  const [connection, connect, disconnect] = useViewerConnection();
+
+  useEffect(() => {
+    if (connection.status !== "connected") {
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+    }
+}, [connection.status]);
 }
